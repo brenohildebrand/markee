ca77cd0fc1b1e1c63f0d2551245d3818a6faee3a
Breno Hildebrand <breno.h1ldebr4nd@gmail.com>
2021-09-17 10:53:44 -0700

    [DEV] App and Root Components Are Ready

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Sidebar } from '@ui/Sidebar/sidebar'
+import { Editor } from '@ui/Editor/editor'
+import styled, { css } from 'styled-components'
 
 function App () {
   return (
-    <div>Hello World!</div>
+    <Wrapper>
+      <Sidebar/>
+      <Editor/>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`${({ theme }) => css`
+    display: flex;
+    flex-direction: row;
+
+    width: 100%;
+    height: 100%;
+`}`
 
 export { App }
